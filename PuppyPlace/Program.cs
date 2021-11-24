88f262c08a1f37009d8c5078e80f528c13124011
Ant <it@me.org>
2021-11-24 17:13:53 -0600

    Add initial contents for Program

--- PuppyPlace/Program.cs
+++ PuppyPlace/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace PuppyPlace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lucy = new Dog("Lucy", 5,"Red Heeler");
+            var clovis = new Dog("Clovis", 1, "Blue Heeler");
 
-Console.WriteLine("Hello, World!");
+
+            var tessandra = new Person("Tessandra");
+            var anthony = new Person("Anthony");
+
+            Console.WriteLine($"Anthony's Id is: {anthony.Id}");
+
+            tessandra.AddDog(lucy);
+            tessandra.AddDog(clovis);
+
+            lucy.AddOwner(tessandra);
+            lucy.AddOwner(anthony);
+            clovis.AddOwner(tessandra);
+
+            tessandra.ShowDogs();
+            lucy.ShowOwners();
+            
+            // Console.WriteLine($"We have a person named: {tessandra.Name}");
+            // Console.WriteLine($"The dog's name is: {lucy.Name}");
+            // Console.WriteLine($"The dog's age is: {lucy.Age}");
+            // Console.WriteLine(lucy.Bark());
+            
+
+            Console.WriteLine("Add another dog? Choose: yes/no");
+            var userChoice = Console.ReadLine();
+
+            if (userChoice != "yes" && userChoice != "no")
+            {
+                Console.WriteLine("Invalid choice. Choose: yes/no");
+            }
+
+            if (userChoice == "yes")
+            {
+                Dog.AddDog();
+            }
+        }
+    }
+}
