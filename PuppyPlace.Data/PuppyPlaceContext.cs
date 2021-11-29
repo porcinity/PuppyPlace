edce8dafd9f8d659b5ad5397cfd12adf3ddbb7f4
Ant <it@me.org>
2021-11-29 15:04:38 -0600

    Create connection string for context

--- PuppyPlace.Data/PuppyPlaceContext.cs
+++ PuppyPlace.Data/PuppyPlaceContext.cs
@@ -6,4 +6,11 @@ namespace PuppyPlace.Data;
 public class PuppyPlaceContext : DbContext
 {
     public DbSet<Person> Persons { get; set; }
+    public DbSet<Dog> Dogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=PuppyPlaceData");
+    }
 }
