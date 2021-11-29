496c23f1e50cf038daba52fcd385592c3fb9990f
Ant <it@me.org>
2021-11-29 17:38:38 -0600

    Refactor Person class

--- PuppyPlace.Domain/Person.cs
+++ PuppyPlace.Domain/Person.cs
@@ -9,13 +9,12 @@ public class Person
     public Guid Id { get; set; }
     [Required]
     public string Name { get; set; }
-    [Column]
-    public List<Dog> Dogs { get; set; }
+    [Column] 
+    public List<Dog> Dogs = new List<Dog>();
     public Person(string name)
     {
         Name = name;
         Id = Guid.NewGuid();
-        Dogs = new List<Dog>();
     }
 
     public void AddDog(Dog dog)
