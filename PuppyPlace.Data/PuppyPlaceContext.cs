87acb2e0d278c433d8463d0bc48e976b979d5874
Ant <it@me.org>
2021-11-29 14:25:07 -0600

    Add Context class to Data project

---
+++ PuppyPlace.Data/PuppyPlaceContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using PuppyPlace.Domain;
+
+namespace PuppyPlace.Data;
+
+public class PuppyPlaceContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+}
