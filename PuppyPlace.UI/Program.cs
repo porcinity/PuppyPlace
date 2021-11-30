d79cb229f799d0fc648c1bf43e52cb81e57dcf5b
Ant <it@me.org>
2021-11-30 16:12:00 -0600

    Show main menu in Main method

--- PuppyPlace.UI/Program.cs
+++ PuppyPlace.UI/Program.cs
@@ -1,36 +1,10 @@
-﻿using PuppyPlace.Data;
-using PuppyPlace.Domain;
-
-namespace PuppyPlace.UI
+﻿namespace PuppyPlace.UI
 {
     class Program
     {
-        private static PuppyPlaceContext _context = new PuppyPlaceContext();
         static void Main(string[] args)
         {
-            // Prompt.ShowMainMenu();
-            _context.Database.EnsureCreated();
-            GetPersons("Before Add");
-            AddPersonDB();
-            GetPersons("After Add");
-            Console.ReadLine();
-        }
-        
-        public static void AddPersonDB()
-        {
-            var person = new Person("Bell Peps");
-            _context.Persons.Add(person);
-            _context.SaveChanges();
-        }
-        
-        public static void GetPersons(string text)
-        {
-            var persons = _context.Persons.ToList();
-            Console.WriteLine($"{text}: Person count is {persons.Count}");
-            foreach (var person in persons)
-            {
-                Console.WriteLine(person.Name);
-            }
+            ConsoleMainMenu.Show();
         }
     }
 }
