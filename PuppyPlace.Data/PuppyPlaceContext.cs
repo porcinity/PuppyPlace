66bf666130435873c712aa5a43888d4ffab76533
Aaron Adrian <aaroncadrian@gmail.com>
2021-11-30 19:31:52 -0800

    Mapped relationship for Person model

--- PuppyPlace.Data/PuppyPlaceContext.cs
+++ PuppyPlace.Data/PuppyPlaceContext.cs
@@ -8,8 +8,13 @@ public class PuppyPlaceContext : DbContext
     public DbSet<Person> Persons { get; set; }
     public DbSet<Dog> Dogs { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>().HasMany(m => m.Dogs).WithOne(m => m.Owner).OnDelete(DeleteBehavior.Cascade);
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite("Filename=./datuh.sqlite");
+        optionsBuilder.UseSqlite("Filename=/Users/aaroncadrian/Repos/PuppyPlace/PuppyPlace.Data/datuh.sqlite");
     }
 }
