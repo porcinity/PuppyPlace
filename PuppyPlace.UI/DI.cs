942a95353a5063f6c40e704072ac7c2f7ab2788d
Ant <it@me.org>
2021-12-01 15:50:18 -0600

    Create DI class

---
+++ PuppyPlace.UI/DI.cs
@@ -0,0 +1,15 @@
+using PuppyPlace.Data;
+using PuppyPlace.Service;
+
+namespace PuppyPlace.UI;
+
+public static class DI
+{
+    public static readonly PuppyPlaceContext PuppyPlaceContext = new ();
+
+    public static readonly PersonUI PersonUi = new PersonUI(new PersonsService(PuppyPlaceContext));
+
+    public static readonly DogsUI DogsUi =
+        new DogsUI(new DogsService(DI.PuppyPlaceContext, new PersonsService(DI.PuppyPlaceContext)),
+            new PersonsService(DI.PuppyPlaceContext));
+}
