9cde5f7e44f908c92e99deacf83378db84565fb2
Ant <it@me.org>
2021-12-01 15:54:36 -0600

    Override OnModelCreating method in Puppy Place Context

--- PuppyPlace.Data/PuppyPlaceContext.cs
+++ PuppyPlace.Data/PuppyPlaceContext.cs
@@ -7,7 +7,10 @@ public class PuppyPlaceContext : DbContext
 {
     public DbSet<Person> Persons { get; set; }
     public DbSet<Dog> Dogs { get; set; }
-
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>().HasMany(m => m.Dogs).WithOne(m => m.Owner).OnDelete(DeleteBehavior.Cascade);
+    }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Filename=./datuh.sqlite");
