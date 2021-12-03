e06000b02ea0a4fa2cd50834975c50f7e7b9d6bd
Ant <it@me.org>
2021-12-03 10:49:26 -0600

    Switch context to use Postgres

--- PuppyPlace.Data/PuppyPlaceContext.cs
+++ PuppyPlace.Data/PuppyPlaceContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using PuppyPlace.Domain;
 
 namespace PuppyPlace.Data;
@@ -7,16 +8,23 @@ public class PuppyPlaceContext : DbContext
 {
     public DbSet<Person> Persons { get; set; }
     public DbSet<Dog> Dogs { get; set; }
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder
-            .Entity<Person>()
-            .HasMany(m => m.Dogs)
-            .WithOne(m => m.Owner)
-            .OnDelete(DeleteBehavior.Cascade);
-    }
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     modelBuilder
+    //         .Entity<Person>()
+    //         .HasMany(m => m.Dogs)
+    //         .WithOne(m => m.Owner)
+    //         .OnDelete(DeleteBehavior.Cascade);
+    //
+    //     modelBuilder
+    //         .Entity<Dog>()
+    //         .HasOne(m => m.Owner);
+    // }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=localhost,1433;Initial Catalog=helpme; User=sa; Password=Strong.Pwd-123");
+        optionsBuilder
+            .UseLazyLoadingProxies()
+            // .UseSqlServer("Server=localhost,1433;Initial Catalog=helpme; User=sa; Password=Strong.Pwd-123")
+            .UseNpgsql(@"Host=localhost;Username=test;Password=test;Database=PuppyPlace");
     }
 }
