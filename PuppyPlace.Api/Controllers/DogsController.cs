48161fb44245a71bba555a64630ed51f2b903f55
Ant <it@me.org>
2021-12-06 14:22:54 -0600

    Add DogsController

---
+++ PuppyPlace.Api/Controllers/DogsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PuppyPlace.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DogsController : ControllerBase
+    {
+        
+    }
+}
