93c9b9415fcf92734479e5638b6f9b275dd96870
Ant <it@me.org>
2021-12-06 16:43:33 -0600

    Commit broken DI class

--- PuppyPlace.UI/DI.cs
+++ PuppyPlace.UI/DI.cs
@@ -7,7 +7,9 @@ public static class DI
 {
     public static readonly PuppyPlaceContext PuppyPlaceContext = new ();
 
-    public static readonly PersonUi PersonUi = new PersonUi(new PersonsService(PuppyPlaceContext), new DogsService(DI.PuppyPlaceContext, new PersonsService(PuppyPlaceContext)));
+    public static readonly PersonUi PersonUi = new PersonUi(
+        new PersonsService(PuppyPlaceContext),
+        new DogsService(DI.PuppyPlaceContext, new PersonsService(PuppyPlaceContext)));
 
     public static readonly DogsUi DogsUi =
         new DogsUi(new DogsService(DI.PuppyPlaceContext, new PersonsService(DI.PuppyPlaceContext)),
