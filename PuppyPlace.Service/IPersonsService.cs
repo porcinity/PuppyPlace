de3e6d03b60307b8c26e0138fc822686598b6420
Ant <it@me.org>
2021-12-06 16:42:48 -0600

    Remove adoptdog method from PersonsService interface

--- PuppyPlace.Service/IPersonsService.cs
+++ PuppyPlace.Service/IPersonsService.cs
@@ -6,7 +6,7 @@ public interface IPersonsService
 {
     Task<List<Person>> FindPersons();
     Task<Person> FindPerson(Guid id);
-    Task AdoptDog(Guid personId, Guid dogId);
+    // Task AdoptDog(Guid personId, Guid dogId);
     Task AddPerson(Person person);
     Task UpdatePerson(Person person);
     Task DeletePerson(Guid id);
