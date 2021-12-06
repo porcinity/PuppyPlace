3b219a93d2ec745fa530582ebff43c403697ad95
Ant <it@me.org>
2021-12-06 16:00:51 -0600

    Add GetDog and GetDogs methods to DogsController

--- PuppyPlace.Api/Controllers/DogsController.cs
+++ PuppyPlace.Api/Controllers/DogsController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using PuppyPlace.Domain;
+using PuppyPlace.Service;
 
 namespace PuppyPlace.Api.Controllers
 {
@@ -6,6 +8,23 @@ namespace PuppyPlace.Api.Controllers
     [ApiController]
     public class DogsController : ControllerBase
     {
+        private readonly IDogsService _dogsService;
+
+        public DogsController(IDogsService dogsService)
+        {
+            _dogsService = dogsService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Dog>>> GetDogs()
+        {
+            return await _dogsService.FindDogs();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Dog>> GetDog(Guid id)
+        {
+            return await _dogsService.FindDog(id);
+        }
         
     }
 }
