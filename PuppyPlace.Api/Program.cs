859f5301f8f7214fa9476e63ec1385f0ff98632d
Ant <it@me.org>
2021-12-06 09:02:13 -0600

    Add PuppyPlaceContext to Api project

--- PuppyPlace.Api/Program.cs
+++ PuppyPlace.Api/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using PuppyPlace.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<PuppyPlaceContext>(opt =>
+    opt.UseNpgsql(connectionString: @"Host=localhost;Username=test;Password=test;Database=PuppyPlace"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
