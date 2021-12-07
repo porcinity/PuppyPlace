2b723580eb78df50cc65a43aac235bde4ce21e57
Ant <it@me.org>
2021-12-06 20:28:49 -0600

    Update IDogsService interface to list DogDTOs

--- PuppyPlace.Service/IDogsService.cs
+++ PuppyPlace.Service/IDogsService.cs
@@ -4,8 +4,9 @@ namespace PuppyPlace.Service;
 
 public interface IDogsService
 {
-    Task<List<Dog>> FindDogs();
+    Task<List<DogDTO>> FindDogs();
     Task<Dog> FindDog(Guid id);
+    Task<Dog> FindDogWithOwner(Guid id);
     Task AddDog(Dog dog);
     Task UpdateDog(Dog dog);
     Task DeleteDog(Guid id);
