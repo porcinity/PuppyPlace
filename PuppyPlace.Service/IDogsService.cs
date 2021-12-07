c00b6a42d405b4e6e6e94578e5970a2fb9b145a6
Ant <it@me.org>
2021-12-07 11:00:29 -0600

    Add Update and Delete methods in IDogsService interface

--- PuppyPlace.Service/IDogsService.cs
+++ PuppyPlace.Service/IDogsService.cs
@@ -7,4 +7,6 @@ public interface IDogsService
     Task<IEnumerable<DogDTO>> FindDogs();
     Task<DogDTO> FindDog(Guid id);
     Task AddDog(DogDTO dogDto);
+    Task UpdateDog(DogDTO dogDto);
+    Task DeleteDog(Guid id);
 }
