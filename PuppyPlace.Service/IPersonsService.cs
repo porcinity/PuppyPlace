cfd38677b58645441d09b8d62d3dcd2bb1a4e735
Ant <it@me.org>
2021-12-07 09:35:45 -0600

    Add UpdatePerson and DeletePerson to IPersonsService

--- PuppyPlace.Service/IPersonsService.cs
+++ PuppyPlace.Service/IPersonsService.cs
@@ -5,5 +5,6 @@ public interface IPersonsService
     Task<IEnumerable<PersonDTO>> FindPersons();
     Task<PersonDTO> FindPerson(Guid id);
     Task AddPerson(PersonDTO personDto);
-    // Task DeletePerson();
+    Task UpdatePerson(PersonDTO personDto);
+    Task DeletePerson(Guid id);
 }
