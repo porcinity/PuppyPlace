09380624f4df89149d593aefd5b68d5379282f1c
Ant <it@me.org>
2021-12-10 14:04:03 -0600

    Add CreatePersonCommand

---
+++ PuppyPlace.Api/Commands/CreatePersonCommand.cs
@@ -0,0 +1,27 @@
+using System.Windows.Input;
+using MediatR;
+using PuppyPlace.Domain;
+using PuppyPlace.Repository;
+
+namespace PuppyPlace.Api.Commands;
+
+public class CreatePersonCommand : IRequest<Guid>
+{
+    public string Name { get; set; }
+}
+
+public class CreatePersonCommandHandler : IRequestHandler<CreatePersonCommand, Guid>
+{
+    private readonly IPersonsRepository _personsRepository;
+
+    public CreatePersonCommandHandler(IPersonsRepository personsRepository)
+    {
+        _personsRepository = personsRepository;
+    }
+    public async Task<Guid> Handle(CreatePersonCommand request, CancellationToken cancellationToken)
+    {
+        var person = new Person(request.Name);
+        await _personsRepository.AddPerson(person);
+        return person.Id;
+    }
+}
