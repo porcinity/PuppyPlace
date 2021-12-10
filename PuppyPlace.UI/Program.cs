f40763fbddef181f5984a9f3436d5275584e0c68
Ant <it@me.org>
2021-12-10 09:02:42 -0600

    Register dependencies

--- PuppyPlace.UI/Program.cs
+++ PuppyPlace.UI/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace PuppyPlace.Ui
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PuppyPlace.Ui
 {
     class Program
     {
         static async Task Main(string[] args)
         {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                    {
+                        services.AddTransient<IPersonUi, PersonUi>();
+                        services.AddTransient<IDogsUi, DogsUi>();
+                        services.AddTransient<IConsoleMenu, ConsoleMenu>();
+                    }
+                );
             await ConsoleMainMenu.Show();
         }
     }
