56adb6d00ea6d09633ab8a296a602d538d78171f
Ant <it@me.org>
2021-12-10 08:37:57 -0600

    Add IDogsRepository to AdoptionService

--- PuppyPlace.Service/AdoptionService.cs
+++ PuppyPlace.Service/AdoptionService.cs
@@ -1,27 +1,30 @@
 using PuppyPlace.Data;
+using PuppyPlace.Repository;
 
 namespace PuppyPlace.Service;
 
 public class AdoptionService : IAdoptionService
 {
     private readonly IPersonsService _personsService;
+    private readonly IDogsRepository _dogsRepository;
     private readonly IDogsService _dogsService;
     private readonly PuppyPlaceContext _context;
 
-    public AdoptionService(PuppyPlaceContext puppyPlaceContext, IDogsService dogsService, IPersonsService personsService)
+    public AdoptionService(PuppyPlaceContext puppyPlaceContext, IDogsService dogsService, IPersonsService personsService, IDogsRepository dogsRepository)
     {
         _personsService = personsService;
+        _dogsRepository = dogsRepository;
         _dogsService = dogsService;
         _context = puppyPlaceContext;
     }
 
     public async Task AdoptDog(Guid personId, Guid dogId)
     {
-        var person = await _personsService.FindPerson(personId);
-        var dog = await _dogsService.FindDog(dogId);
+        // var person = await _personsService.FindPerson(personId);
+        var dog = await _dogsRepository.FindDog(dogId);
         
         dog.OwnerId = personId;
-        await _dogsService.UpdateDog(dog);
+        await _dogsRepository.UpdateDog(dog);
         await _context.SaveChangesAsync();
     }
 }
