fc81ad590629587ab982b0125ce349fbbca51b86
Ant <it@me.org>
2021-12-10 19:44:22 -0600

    Add GetDogByIdQuery to Services project

---
+++ PuppyPlace.Services/Dogs/Queries/GetDogByIdQuery.cs
@@ -0,0 +1,24 @@
+using MediatR;
+using PuppyPlace.Domain;
+using PuppyPlace.Repository;
+
+namespace PuppyPlace.Services.Dogs.Queries;
+
+public class GetDogByIdQuery : IRequest<Dog>
+{
+    public Guid Id { get; set; }
+}
+
+public class GetDogByIdQueryHandler : IRequestHandler<GetDogByIdQuery, Dog>
+{
+    private readonly IDogsRepository _dogsRepository;
+
+    public GetDogByIdQueryHandler(IDogsRepository dogsRepository)
+    {
+        _dogsRepository = dogsRepository;
+    }
+    public async Task<Dog> Handle(GetDogByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await _dogsRepository.FindDog(request.Id);
+    }
+}
