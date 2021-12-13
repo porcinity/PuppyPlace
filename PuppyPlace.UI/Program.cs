8ba94694b83b81182adfd2d73325a5b6892ad0ff
Ant <it@me.org>
2021-12-13 17:06:57 -0600

    Use original static method in Program.cs for Console Project

--- PuppyPlace.UI/Program.cs
+++ PuppyPlace.UI/Program.cs
@@ -10,20 +10,7 @@ namespace PuppyPlace.Ui
     {
         static async Task Main(string[] args)
         {
-            var services = new ServiceCollection();
-            services.AddSingleton<IConsoleMenu, ConsoleMenu>();
-            services.AddSingleton<IMainMenu, MainMenu>();
-            services.AddTransient<IPersonUi, PersonUi>();
-            services.AddTransient<IPersonsRepository, PersonsRepository>();
-            services.AddTransient<IDogsRepository, DogsRepository>();
-            services.AddTransient<IAdoptionService, AdoptionService>();
-            services.AddTransient<IDogsService, DogsService>();
-            services.AddTransient<IPersonsService, PersonsService>();
-            services.AddTransient<IDogsUi, DogsUi>();
-            services.AddTransient<PuppyPlaceContext>();
-            var serviceProvider = services.BuildServiceProvider();
-            var menu = serviceProvider.GetService<IConsoleMenu>();
-            await menu.Show();
+            await ConsoleMainMenu.Show();
         }
     }
 }
