ab6256669646ccc02e4e6a2f7bb5a4fe6a371ce3
Ant <it@me.org>
2021-12-13 17:06:31 -0600

    Restore IoC container

--- PuppyPlace.UI/DI.cs
+++ PuppyPlace.UI/DI.cs
@@ -6,18 +6,18 @@ namespace PuppyPlace.Ui;
 
 public static class DI
 {
-    // public static readonly PuppyPlaceContext PuppyPlaceContext = new ();
-    //
-    // public static readonly IPersonsRepository PersonsRepository = new PersonsRepository(PuppyPlaceContext);
-    // public static readonly IDogsRepository DogsRepository = new DogsRepository(PuppyPlaceContext);
-    //
-    // public static readonly IPersonsService PersonsService = new PersonsService(PersonsRepository);
-    // public static readonly IDogsService DogsService = new DogsService(DogsRepository);
-    //
-    // public static readonly PersonUi PersonUi = new PersonUi(
-    //     new PersonsRepository(PuppyPlaceContext),
-    //     new DogsRepository(PuppyPlaceContext),
-    //     new AdoptionService(PuppyPlaceContext, DogsService, PersonsService, DogsRepository));
-    //
-    // public static readonly DogsUi DogsUi = new DogsUi( new DogsRepository(PuppyPlaceContext), new PersonsRepository(PuppyPlaceContext));
+    public static readonly PuppyPlaceContext PuppyPlaceContext = new ();
+    
+    public static readonly IPersonsRepository PersonsRepository = new PersonsRepository(PuppyPlaceContext);
+    public static readonly IDogsRepository DogsRepository = new DogsRepository(PuppyPlaceContext);
+    
+    public static readonly IPersonsService PersonsService = new PersonsService(PersonsRepository);
+    public static readonly IDogsService DogsService = new DogsService(DogsRepository);
+    
+    public static readonly PersonUi PersonUi = new PersonUi(
+        new PersonsRepository(PuppyPlaceContext),
+        new DogsRepository(PuppyPlaceContext),
+        new AdoptionService(PuppyPlaceContext, DogsRepository));
+    
+    public static readonly DogsUi DoggieUi = new DogsUi(DogsRepository, PersonsRepository);
 }
