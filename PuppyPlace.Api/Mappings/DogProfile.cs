8215a96e7de6bf27b42e95c6976a6489f50573cd
Ant <it@me.org>
2021-12-14 11:15:01 -0600

    Add DogProfile class for Automapper

---
+++ PuppyPlace.Api/Mappings/DogProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using PuppyPlace.Api.Dtos;
+using PuppyPlace.Domain;
+
+namespace PuppyPlace.Api.Mappings;
+
+public class DogProfile : Profile
+{
+    public DogProfile()
+    {
+        CreateMap<Person, DogOwnerDto>();
+        CreateMap<Dog, GetDogDto>();
+    }
+}
