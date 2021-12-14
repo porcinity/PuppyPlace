be87ee8d46e180a1379fc6cc3e9bee401eabf02d
Aaron Adrian <aaroncadrian@gmail.com>
2021-12-14 14:54:43 -0800

    Add owned type for name

--- PuppyPlace.Data/PuppyPlaceContext.cs
+++ PuppyPlace.Data/PuppyPlaceContext.cs
@@ -9,22 +9,26 @@ public class PuppyPlaceContext : DbContext
     public DbSet<Person> Persons { get; set; }
     public DbSet<Dog> Dogs { get; set; }
 
+    public DbSet<Client> Clients { get; set; }
+
     public PuppyPlaceContext(DbContextOptions<PuppyPlaceContext> options) : base(options)
     {
-        
+
     }
     public PuppyPlaceContext()
     {
-        
+
     }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfiguration(new PersonConfiguration());
+
+        builder.Entity<Client>().OwnsOne(m => m.Name).Property(m => m.Value).HasColumnName("Name");
     }
+
     public class PostgresContext : PuppyPlaceContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseNpgsql(@"Host=localhost;Username=test;Password=test;Database=TheNewPuppyPlace");
     }
-    
 }
