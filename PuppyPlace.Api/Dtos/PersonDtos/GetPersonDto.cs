0af790e57e2e64ed49af14e043bd4c9a313b4826
Ant <it@me.org>
2021-12-15 16:30:46 -0600

    Update GetPersonDto to include Age

--- PuppyPlace.Api/Dtos/PersonDtos/GetPersonDto.cs
+++ PuppyPlace.Api/Dtos/PersonDtos/GetPersonDto.cs
@@ -5,5 +5,6 @@ namespace PuppyPlace.Api.Dtos;
 public class GetPersonDto
 {
     public string Name { get; set; }
+    public int Age { get; set; }
     public IEnumerable<PersonDogsDto> Dogs { get; set; }
 }
