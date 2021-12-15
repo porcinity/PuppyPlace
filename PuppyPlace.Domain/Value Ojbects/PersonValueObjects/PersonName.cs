dc35d4a0264cfbb67ce0d552a11ec13597c25d06
Ant <it@me.org>
2021-12-15 10:40:43 -0600

    Add custom validation to PersonName constructor

--- PuppyPlace.Domain/Value Ojbects/PersonValueObjects/PersonName.cs
+++ PuppyPlace.Domain/Value Ojbects/PersonValueObjects/PersonName.cs
@@ -1,11 +1,23 @@
 namespace PuppyPlace.Domain.Value_Ojbects.PersonValueObjects;
 
-public class PersonName
+public record PersonName
 {
     private readonly string _value;
 
     public PersonName(string value)
     {
+        if (string.IsNullOrEmpty(value))
+        {
+            throw new EmptyNameException();
+        }
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new WhiteSpaceNameException();
+        }
+        if (value.Length > 30)
+        {
+            throw new NameTooLongException();
+        }
         _value = value;
     }
     public string Value => _value;
@@ -13,6 +25,28 @@ public class PersonName
     {
         return _value;
     }
-
     public static implicit operator PersonName(string value) => new PersonName(value);
+}
+
+public class EmptyNameException : Exception
+{
+    public EmptyNameException() : base("Name cannot be empty.")
+    {
+    }
+}
+
+public class NameTooLongException : Exception
+{
+    public NameTooLongException() : base("Name is too long.")
+    {
+        
+    }
+}
+
+public class WhiteSpaceNameException : Exception
+{
+    public WhiteSpaceNameException() : base("Name cannot contain whitespace.")
+    {
+        
+    }
 }
