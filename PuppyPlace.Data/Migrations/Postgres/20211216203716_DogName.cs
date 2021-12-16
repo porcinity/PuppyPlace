62bea85938842c18d76a0b771a47de9840608723
Ant <it@me.org>
2021-12-16 15:43:17 -0600

    Add migrations

---
+++ PuppyPlace.Data/Migrations/Postgres/20211216203716_DogName.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PuppyPlace.Data.Migrations.Postgres
+{
+    public partial class DogName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Dogs");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Dogs",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ PuppyPlace.Data/Migrations/Postgres/20211216213204_PersonIdRevert.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PuppyPlace.Data.Migrations.Postgres
+{
+    public partial class PersonIdRevert : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dogs_Persons_OwnerId1",
+                table: "Dogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Dogs_OwnerId1",
+                table: "Dogs");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId1",
+                table: "Dogs");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dogs_OwnerId",
+                table: "Dogs",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dogs_Persons_OwnerId",
+                table: "Dogs",
+                column: "OwnerId",
+                principalTable: "Persons",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dogs_Persons_OwnerId",
+                table: "Dogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Dogs_OwnerId",
+                table: "Dogs");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId1",
+                table: "Dogs",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dogs_OwnerId1",
+                table: "Dogs",
+                column: "OwnerId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dogs_Persons_OwnerId1",
+                table: "Dogs",
+                column: "OwnerId1",
+                principalTable: "Persons",
+                principalColumn: "Id");
+        }
+    }
+}
