491f83b637ac7f9c1da148c8dd87980b6ddb27b5
Ant <it@me.org>
2021-12-16 14:26:48 -0600

    Use DogName in CreateDogCommand

--- PuppyPlace.Services/Dogs/Commands/CreateDogCommand.cs
+++ PuppyPlace.Services/Dogs/Commands/CreateDogCommand.cs
@@ -1,14 +1,15 @@
 using MediatR;
 using PuppyPlace.Domain;
+using PuppyPlace.Domain.Value_Objects.DogValueObjects;
 using PuppyPlace.Repository;
 
 namespace PuppyPlace.Services.Dogs.Commands;
 
 public class CreateDogCommand : IRequest<Guid>
 {
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public string Breed { get; set; }
+    public string Name { get; }
+    public int Age { get; }
+    public string Breed { get; }
 }
 
 public class CreateDogCommandHandler : IRequestHandler<CreateDogCommand, Guid>
@@ -21,7 +22,8 @@ public class CreateDogCommandHandler : IRequestHandler<CreateDogCommand, Guid>
     }
     public async Task<Guid> Handle(CreateDogCommand request, CancellationToken cancellationToken)
     {
-        var dog = new Dog(request.Name, request.Age, request.Breed);
+        var dogName = DogName.Create(request.Name);
+        var dog = new Dog(dogName, request.Age, request.Breed);
         await _dogsRepository.AddDog(dog);
         return dog.Id;
     }
