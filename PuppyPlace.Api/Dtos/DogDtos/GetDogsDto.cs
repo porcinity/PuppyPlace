d509fa36bae30d8e78a0a8dd0917a1d7a6a54c32
Ant <it@me.org>
2021-12-17 11:43:24 -0600

    Update naming of GetDogDto factory method in GetDogsDto.cs

--- PuppyPlace.Api/Dtos/DogDtos/GetDogsDto.cs
+++ PuppyPlace.Api/Dtos/DogDtos/GetDogsDto.cs
@@ -7,12 +7,12 @@ public class GetDogsDto
     private List<GetDogDto> _dogs { get; } = new List<GetDogDto>();
     public IEnumerable<GetDogDto> Dogs => _dogs;
 
-    public static GetDogsDto Create(IEnumerable<Dog> dogs)
+    public static GetDogsDto FromDogs(IEnumerable<Dog> dogs)
     {
         var dto = new GetDogsDto();
         foreach (var dog in dogs)
         {
-            var dogDto = GetDogDto.Create(dog);
+            var dogDto = GetDogDto.FromDog(dog);
             dto._dogs.Add(dogDto);
         }
 
