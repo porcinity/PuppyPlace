4deaea5e0a5f69be07bb28276df4e45065abbf09
Ant <it@me.org>
2021-12-16 18:08:24 -0600

    Update PutDogCommand to use new Update method

--- PuppyPlace.Services/Dogs/Commands/PutDogCommand.cs
+++ PuppyPlace.Services/Dogs/Commands/PutDogCommand.cs
@@ -1,4 +1,5 @@
 using MediatR;
+using PuppyPlace.Domain.Value_Objects.DogValueObjects;
 using PuppyPlace.Repository;
 
 namespace PuppyPlace.Services.Dogs.Commands;
@@ -22,9 +23,10 @@ public class PutDogCommandHandler : IRequestHandler<PutDogCommand, Guid>
     public async Task<Guid> Handle(PutDogCommand request, CancellationToken cancellationToken)
     {
         var dog = await _dogsRepository.FindDog(request.Id);
-        dog.Name = request.Name;
-        dog.Age = request.Age;
-        dog.Breed = request.Breed;
+        var newName = DogName.Create(request.Name);
+        var newAge = DogAge.Create(request.Age);
+        var newBreed = DogBreed.Create(request.Breed);
+        dog.Update(newName, newAge, newBreed);
         await _dogsRepository.UpdateDog(dog);
         return dog.Id;
     }
