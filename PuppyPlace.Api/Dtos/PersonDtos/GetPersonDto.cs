b77bd987ec5f03eeefe9b4161c55934567c07ce2
Ant <it@me.org>
2021-12-17 00:27:25 -0600

    Add factory method to GetPersonDto

--- PuppyPlace.Api/Dtos/PersonDtos/GetPersonDto.cs
+++ PuppyPlace.Api/Dtos/PersonDtos/GetPersonDto.cs
@@ -4,7 +4,17 @@ namespace PuppyPlace.Api.Dtos;
 
 public class GetPersonDto
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public int Age { get; set; }
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public int Age { get; init; }
+
+    public static GetPersonDto Create(Person person)
+    {
+        return new GetPersonDto
+        {
+            Id = person.Id,
+            Name = person.Name,
+            Age = person.Age.Value
+        };
+    }
 }
