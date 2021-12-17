2027631a870d2bc4d29cf382fa5cfa810163d06f
Ant <it@me.org>
2021-12-16 18:35:54 -0600

    Add Id to PostDogDto

--- PuppyPlace.Api/Dtos/DogDtos/PostDogDto.cs
+++ PuppyPlace.Api/Dtos/DogDtos/PostDogDto.cs
@@ -2,6 +2,7 @@ namespace PuppyPlace.Api.Dtos;
 
 public class PostDogDto
 {
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
     public string Breed { get; set; }
