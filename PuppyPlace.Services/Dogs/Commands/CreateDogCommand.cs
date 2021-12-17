6c69dde060b8f2683c25f7e3555111c99f1a54e8
Ant <it@me.org>
2021-12-16 18:27:45 -0600

    Return Dog from CreateDogCommand

--- PuppyPlace.Services/Dogs/Commands/CreateDogCommand.cs
+++ PuppyPlace.Services/Dogs/Commands/CreateDogCommand.cs
@@ -5,14 +5,14 @@ using PuppyPlace.Repository;
 
 namespace PuppyPlace.Services.Dogs.Commands;
 
-public class CreateDogCommand : IRequest<Guid>
+public class CreateDogCommand : IRequest<Dog>
 {
     public string Name { get; set; }
     public int Age { get; set; }
     public string Breed { get; set; }
 }
 
-public class CreateDogCommandHandler : IRequestHandler<CreateDogCommand, Guid>
+public class CreateDogCommandHandler : IRequestHandler<CreateDogCommand, Dog>
 {
     private readonly IDogsRepository _dogsRepository;
 
@@ -20,13 +20,13 @@ public class CreateDogCommandHandler : IRequestHandler<CreateDogCommand, Guid>
     {
         _dogsRepository = dogsRepository;
     }
-    public async Task<Guid> Handle(CreateDogCommand request, CancellationToken cancellationToken)
+    public async Task<Dog> Handle(CreateDogCommand request, CancellationToken cancellationToken)
     {
         var dogName = DogName.Create(request.Name);
         var dogAge = DogAge.Create(request.Age);
         var dogBreed = DogBreed.Create(request.Breed);
         var dog = new Dog(dogName, dogAge, dogBreed);
         await _dogsRepository.AddDog(dog);
-        return dog.Id;
+        return dog;
     }
 }
