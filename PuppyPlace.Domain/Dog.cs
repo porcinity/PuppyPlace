07d364cf3ca29dcfb6b7f75b81733309fe75f7e1
Ant <it@me.org>
2021-12-17 12:57:00 -0600

    Remove unused using statements in Dog model

--- PuppyPlace.Domain/Dog.cs
+++ PuppyPlace.Domain/Dog.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using PuppyPlace.Domain.Value_Objects.DogValueObjects;
 
 namespace PuppyPlace.Domain;
