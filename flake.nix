68337987ddb417321b0fa7a329e701c8bbd2fc60
Ant <it@me.org>
2021-12-26 09:26:12 -0600

    Change indentation in flake file

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Initial Flake file";
+  description = "PuppyPlace Flake";
 
   inputs = {
 
@@ -15,37 +15,39 @@
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              postgresql
-              powershell
-              (with dotnetCorePackages; combinePackages [
-                dotnet-sdk_6
-                mono
-                dotnetPackages.Nuget
-              ])
-            ];
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            postgresql
+            powershell
+            (with dotnetCorePackages; combinePackages [
+              dotnet-sdk_6
+              mono
+              dotnetPackages.Nuget
+            ])
+          ];
 
-            # shellHook = ''
-            #   export PGHOST=./tmp/postgres
-            #   export PGDATA=$PGHOST/data
-            #   export PGDATABASE=postgres
-            #   export PGLOG=$PGHOST/postgres.log
+          shellHook = ''
+              export PGHOST=./tmp/postgres
+              export PGDATA=$PGHOST/data
+              export PGDATABASE=postgres
+              export PGLOG=$PGHOST/postgres.log
+          '';
 
-            #   mkdir -p $PGHOST
+          #   mkdir -p $PGHOST
 
-            #   if [ ! -d $PGDATA ]; then
-            #   initdb --auth=trust --no-locale --encoding=UTF8
-            #   fi
+          #   if [ ! -d $PGDATA ]; then
+          #   initdb --auth=trust --no-locale --encoding=UTF8
+          #   fi
 
-            #   if ! pg_ctl status
-            #   then
-            #   pg_ctl start -l $PGLOG -o "--unix_socket_directories='$PGHOST' --listen_addresses='''"
-            #   fi
-           # '';
-          };
-        });
+          #   if ! pg_ctl status
+          #   then
+          #   pg_ctl start -l $PGLOG -o "--unix_socket_directories='$PGHOST' --listen_addresses='''"
+          #   fi
+          # '';
+          # https://jamesmead.org/blog/2020-11-29-multiple-rails-development-environments-using-nix-shell
+        };
+      });
 }
