f97c4e40524259f663b2bd0a98503dd7b80b174e
Ant <it@me.org>
2022-02-10 13:43:32 -0600

    Update person repo interface

--- PuppyPlace.Repository/IPersonsRespository.cs
+++ PuppyPlace.Repository/IPersonsRespository.cs
@@ -7,6 +7,6 @@ public interface IPersonsRepository
     Task<IEnumerable<Person>> FindPersons();
     Task<Person> FindPerson(Guid id);
     Task AddPerson(Person person);
-    Task UpdatePerson(Person person);
+    Task<Person> UpdatePerson(Person person);
     Task DeletePerson(Guid id);
 }
